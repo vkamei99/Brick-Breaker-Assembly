79aace9dbf9b47e75756c4426a398291a8915446
Viktor Kamei Mota <vikmotakamei@gmail.com>
2024-05-29 19:35:47 -0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,3 +1,242 @@
+segment code
+..start:
+    mov         ax,data
+    mov         ds,ax
+    mov         ax,stack
+    mov         ss,ax
+    mov         sp,stacktop
+
+; Salvar modo corrente de video
+    mov         ah,0Fh
+    int         10h
+    mov         [modo_anterior],al   
+
+; Alterar modo de video para gráfico 640x480 16 cores
+    mov         al,12h
+    mov         ah,0
+    int         10h
+        
+; Limpa si, di, e define onde a bolinha começa
+    xor si, si
+    xor di, di
+    mov cx, 0800h
+    mov si, 319
+    mov di, 240
+
+    mov     byte [cor], preto    ; apaga title
+    mov     ax, 200
+    push    ax
+    mov     ax, 240
+    push    ax
+    mov     ax, 430
+    push    ax
+    mov     ax, 255
+    push    ax
+    call    rect
+
+    ;escrever o cabecalho
+    mov     cx,20			;numero de caracteres
+    mov     bx,0
+    mov     dh,14			;linha 0-29
+    mov     dl,29 			;coluna 0-79
+	mov	    byte[cor],branco
+
+show_title:
+	call    cursor
+    mov     al,[bx+title]
+	call    caracter
+    inc     bx	                ;proximo caracter
+	inc 	dl	                ;avanca a coluna
+    loop    show_title
+
+inicio:
+    mov ah,00h
+    int 16h
+
+    cmp al,0Dh ;'enter' -> start no jogo
+    je desenha_layout 
+
+    jmp inicio
+
+desenha_layout:
+    mov     byte [cor], preto    ; apaga title
+    mov     ax, 230
+    push    ax
+    mov     ax, 240
+    push    ax
+    mov     ax, 390
+    push    ax
+    mov     ax, 255
+    push    ax
+    call    rect
+
+    mov     byte[cor],branco_intenso    ; esquerda
+    mov     ax,0
+    push        ax
+    mov     ax,0
+    push        ax
+    mov     ax,0
+    push        ax
+    mov     ax,479
+    push        ax
+    call        line
+
+    mov     byte[cor],branco_intenso    ; cima
+    mov     ax,0
+    push        ax
+    mov     ax,479
+    push        ax
+    mov     ax,639
+    push        ax
+    mov     ax,479
+    push        ax
+    call        line
+
+    mov     byte[cor],branco_intenso    ; direita
+    mov     ax,639
+    push        ax
+    mov     ax,0
+    push        ax
+    mov     ax,639
+    push        ax
+    mov     ax,479
+    push        ax
+    call        line 
+
+desenha_blocos:    ;Largura: 95, Altura: 20, Espaçamento: 10
+    ;1,1
+    mov     byte [cor], magenta_claro   
+    mov     ax, 10
+    push    ax
+    mov     ax, 450
+    push    ax
+    mov     ax, 105
+    push    ax
+    mov     ax, 470
+    push    ax
+    call    rect
+    ;2,1 
+    mov     byte [cor], vermelho          
+    mov     ax, 10
+    push    ax
+    mov     ax, 420
+    push    ax
+    mov     ax, 105
+    push    ax
+    mov     ax, 440
+    push    ax
+    call    rect
+    ;1,2 
+    mov     byte [cor], azul              
+    mov     ax, 115
+    push    ax
+    mov     ax, 450
+    push    ax
+    mov     ax, 210
+    push    ax
+    mov     ax, 470
+    push    ax
+    call    rect
+    ;2,2
+    mov     byte [cor], amarelo           
+    mov     ax, 115
+    push    ax
+    mov     ax, 420
+    push    ax
+    mov     ax, 210
+    push    ax
+    mov     ax, 440
+    push    ax
+    call    rect
+    ;1,3
+    mov     byte [cor], cyan   
+    mov     ax, 220
+    push    ax
+    mov     ax, 450
+    push    ax
+    mov     ax, 315
+    push    ax
+    mov     ax, 470
+    push    ax
+    call    rect
+    ;2,3
+    mov     byte [cor], verde_claro   
+    mov     ax, 220
+    push    ax
+    mov     ax, 420
+    push    ax
+    mov     ax, 315
+    push    ax
+    mov     ax, 440
+    push    ax
+    call    rect
+    ;1,4
+    mov     byte [cor], verde_claro   
+    mov     ax, 325
+    push    ax
+    mov     ax, 450
+    push    ax
+    mov     ax, 420
+    push    ax
+    mov     ax, 470
+    push    ax
+    call    rect
+    ;2,4
+    mov     byte [cor], cyan   
+    mov     ax, 325
+    push    ax
+    mov     ax, 420
+    push    ax
+    mov     ax, 420
+    push    ax
+    mov     ax, 440
+    push    ax
+    call    rect
+    ;1,5
+    mov     byte [cor], amarelo   
+    mov     ax, 430
+    push    ax
+    mov     ax, 450
+    push    ax
+    mov     ax, 525
+    push    ax
+    mov     ax, 470
+    push    ax
+    call    rect
+    ;2,5
+    mov     byte [cor], azul   
+    mov     ax, 430
+    push    ax
+    mov     ax, 420
+    push    ax
+    mov     ax, 525
+    push    ax
+    mov     ax, 440
+    push    ax
+    call    rect
+    ;1,6
+    mov     byte [cor], vermelho   
+    mov     ax, 535
+    push    ax
+    mov     ax, 450
+    push    ax
+    mov     ax, 630
+    push    ax
+    mov     ax, 470
+    push    ax
+    call    rect
+    ;2,6
+    mov     byte [cor], magenta_claro 
+    mov     ax, 535
+    push    ax
+    mov     ax, 420
+    push    ax
+    mov     ax, 630
+    push    ax
+    mov     ax, 440
+    push    ax
+    call    rect
+
 main:
     ; Desenhar a bola
     mov     byte[cor],azul  
