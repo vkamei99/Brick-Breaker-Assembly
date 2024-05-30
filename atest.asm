ebc14780055f54499595d1de1751f1858e523771
Viktor Kamei Mota <vikmotakamei@gmail.com>
2024-05-30 18:57:51 -0300

    Colisão feito de um jeito bem merda

--- atest.asm
+++ atest.asm
@@ -314,31 +314,7 @@ esperar_entrada:
     jmp esperar_entrada
 jmp_salva:
     jmp salva_inicio
-colisao_blocos:
-    cmp di, 440
-    jg jmp_boost2
 
-    cmp di, 420
-    jl jmp_boost2
-
-    cmp si, 430
-    jl jmp_boost2
-
-    cmp si, 525
-    jg jmp_boost2
-
-    mov byte [cor], preto
-    mov ax, 430    
-    push ax
-    mov ax, 420    
-    push ax
-    mov ax, 525    
-    push ax
-    mov ax, 440    
-    push ax
-    call rect
-
-    neg word [vy]
 pause:
     cmp word[vx], 0
     je unpause      
@@ -453,6 +429,164 @@ salva_inicio: ;redefine as variaveis para seus valores iniciais
     mov     word[x_barra_end], 370
     mov     word[y_barra],  40     
     jmp ..start
+
+colisao_blocos:
+    ;bloco 1
+    cmp di, 440
+    jg bloco2
+
+    cmp di, 420
+    jl bloco2
+
+    cmp si, 10
+    jl bloco2
+
+    cmp si, 105
+    jg bloco2
+
+    mov byte [cor], preto
+    mov ax, 10    
+    push ax
+    mov ax, 420    
+    push ax
+    mov ax, 105    
+    push ax
+    mov ax, 440    
+    push ax
+    call rect
+
+    neg word [vy]
+
+bloco2:
+    cmp di, 440
+    jg bloco3
+
+    cmp di, 420
+    jl bloco3
+
+    cmp si, 115
+    jl bloco3
+
+    cmp si, 210
+    jg bloco3
+
+    mov byte [cor], preto
+    mov ax, 115    
+    push ax
+    mov ax, 420    
+    push ax
+    mov ax, 210    
+    push ax
+    mov ax, 440    
+    push ax
+    call rect
+
+    neg word [vy]
+bloco3:
+    cmp di, 440
+    jg bloco4
+
+    cmp di, 420
+    jl bloco4
+
+    cmp si, 220
+    jl bloco4
+
+    cmp si, 315
+    jg bloco4
+
+    mov byte [cor], preto
+    mov ax, 220    
+    push ax
+    mov ax, 420    
+    push ax
+    mov ax, 315    
+    push ax
+    mov ax, 440    
+    push ax
+    call rect
+
+    neg word [vy]
+
+bloco4:
+    cmp di, 440
+    jg bloco5
+
+    cmp di, 420
+    jl bloco5
+
+    cmp si, 325
+    jl bloco5
+
+    cmp si, 420
+    jg bloco5
+
+    mov byte [cor], preto
+    mov ax, 325    
+    push ax
+    mov ax, 420    
+    push ax
+    mov ax, 420    
+    push ax
+    mov ax, 440    
+    push ax
+    call rect
+
+    neg word [vy]
+bloco5:
+    cmp di, 440
+    jg bloco6
+
+    cmp di, 420
+    jl bloco6
+
+    cmp si, 430
+    jl bloco6
+
+    cmp si, 525
+    jg bloco6
+
+    mov byte [cor], preto
+    mov ax, 430    
+    push ax
+    mov ax, 420    
+    push ax
+    mov ax, 525    
+    push ax
+    mov ax, 440    
+    push ax
+    call rect
+
+    neg word [vy]
+
+bloco6:
+    cmp di, 440
+    jg jmp_boost3
+
+    cmp di, 420
+    jl jmp_boost3
+
+    cmp si, 535
+    jl jmp_boost3
+
+    cmp si, 630
+    jg jmp_boost3
+
+    mov byte [cor], preto
+    mov ax, 535    
+    push ax
+    mov ax, 420    
+    push ax
+    mov ax, 630    
+    push ax
+    mov ax, 440    
+    push ax
+    call rect
+
+    neg word [vy]
+jmp_boost3:
+    jmp main
+
 ;===============================================================================================================;
 
 l4:
